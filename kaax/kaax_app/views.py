0ef48880c3723689719d3847926e93a08fe031f9
Ludwin Blanco <lblancoorellana@gmail.com>
2023-06-09 11:17:01 -0600

    arrglando graficas

--- kaax/kaax_app/views.py
+++ kaax/kaax_app/views.py
@@ -18,12 +18,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
 from joblib import dump, load
-from datetime import datetime
+from datetime import datetime,timedelta
 from kaax_app.soporte import reporte
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill, Font, Border, Side
 import matplotlib.pyplot as plt
 import pdfkit
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.lib.colors import HexColor
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.piecharts import Pie
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.barcharts import VerticalBarChart
+from reportlab.lib.colors import HexColor
+from reportlab.lib import colors
+from reportlab.graphics.shapes import Rect
 
 @csrf_exempt
 def entrenamiento_csv(request):
@@ -402,39 +412,274 @@ def reporte_graficas(request):
     
     datos = reporte(1, request.data[ 'fechaInicial'], request.data['fechaFinal'])
 
+    total_registros = datos["total_registros"]
+    total_fraud = datos["total_fraud"]
+    porcentaje_fraud = datos["porcentaje_fraud"]
+    total_legit = datos["total_legit"]
+    porcentaje_legit = datos["porcentaje_legit"]
+    fraud_por_hora = datos["fraud_por_hora"]
+    legit_por_hora = datos["legit_por_hora"]
+    fraud_por_dia_semana = datos["fraud_por_dia_semana"]
+    legit_por_dia_semana = datos["legit_por_dia_semana"]
+    fraud_por_semana = datos["fraud_por_semana"]
+    legit_por_semana = datos["legit_por_semana"]
+    fraud_por_mes = datos["fraud_por_mes"]
+    legit_por_mes = datos["legit_por_mes"]
+
+    ancho_pagina = 600
+    alto_pagina = 800
+    ancho_grafico = ancho_pagina / 3
+    alto_grafico = alto_pagina / 3
+
+    c = canvas.Canvas('reporteria/graficos.pdf', pagesize=letter)
+
+    # Inicializar el índice
+    index = 0
+
+    x = 0
+    y = 0
+
+    # Agregar el objeto Rect al Drawing
+    drawing = Drawing(ancho_grafico, alto_grafico)
+    borde = Rect(0, 0, ancho_grafico, alto_grafico)
+    borde.strokeColor = colors.black
+    borde.strokeWidth = 1
+    drawing.add(borde)
+
+    pie = Pie()
+    pie.x = ancho_grafico / 2
+    pie.y = alto_grafico / 2
+    pie.width = ancho_grafico
+    pie.height = alto_grafico
+    pie.data = [total_fraud, total_legit]
+    pie.labels = ['Total fraudes', 'Total Legitimas']
+    pie.slices.strokeWidth = 0.5
+    pie.slices.fontName = 'Helvetica-Bold'
+    pie.slices.fontSize = 8
+    pie.slices.labelRadius = 0.7
+    pie.slices.strokeColor = colors.white
+
+    drawing.add(pie)
+    drawing.drawOn(c, x, y)
+
+    x += ancho_grafico
+
+    drawing2 = Drawing(ancho_grafico, alto_grafico)
+
+    pie2 = Pie()
+    pie2.x = ancho_grafico / 2
+    pie2.y = alto_grafico / 2
+    pie2.width = ancho_grafico
+    pie2.height = alto_grafico
+    pie2.data = [porcentaje_fraud, porcentaje_legit]
+    pie2.labels = ['Porcentaje fraudes', 'Porcentaje Legitimas']
+    pie2.slices.strokeWidth = 0.5
+    pie2.slices.fontName = 'Helvetica-Bold'
+    pie2.slices.fontSize = 8
+    pie2.slices.labelRadius = 0.7
+    pie2.slices.strokeColor = colors.white
+
+    drawing2.add(pie2)
+    drawing2.drawOn(c, x, y)
+
+    x += ancho_grafico
+
+    categorias = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+    valoresf = []
+    valoresl = []
+
+    for hora in categorias:
+        contador = 0
+        for item in fraud_por_hora:
+            if item['hora'] == hora:
+                valoresf.append(item['count'])
+                contador = 1
+        if contador == 0:
+            valoresf.append(0)
+
+    for hora in categorias:
+        contador = 0
+        for item in legit_por_hora:
+            if item['hora'] == hora:
+                valoresl.append(item['count'])
+                contador = 1
+        if contador == 0:
+            valoresl.append(0)
+
+    drawing3 = Drawing(ancho_grafico, alto_grafico)
+    data = [valoresl, valoresf]
+    chart = VerticalBarChart()
+    chart.x = ancho_grafico / 4
+    chart.y = alto_grafico / 4
+    chart.width = ancho_grafico / 2
+    chart.height = alto_grafico / 2
+    chart.data = data
+    chart.categoryAxis.categoryNames = [str(h) for h in categorias]
+    chart.bars[0].fillColor = colors.red
+    chart.bars[1].fillColor = colors.green
+
+    drawing3.add(chart)
+    drawing3.drawOn(c, x, y)
+
+    c.showPage()
+    # c.save()
+
     
-    # Obtener los datos de "fraud" y "legit" por hora del día del diccionario de resultados
-    fraud_por_hora = datos['fraud_por_hora']
-    legit_por_hora = datos['legit_por_hora']
+    categorias = [1, 2, 3, 4, 5, 6, 7]
+    categoriasN = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
+    valoresf = []
+    valoresl = []
 
-    # Obtener las horas y los recuentos de "fraud" y "legit" por hora del día
-    horas = [item['hora'] for item in fraud_por_hora]
-    fraud_counts = [item['count'] for item in fraud_por_hora]
-    legit_counts = [item['count'] for item in legit_por_hora]
+    for dia in categorias:
+        contador = 0
+        for item in fraud_por_dia_semana:
+            if item['dia_semana'] == dia:
+                valoresf.append(item['count'])
+                contador = 1
+        if contador == 0:
+            valoresf.append(0)
 
-    # Crear el gráfico de barras
-    plt.figure(figsize=(10, 6))
-    plt.bar(horas, fraud_counts, label='Fraud')
-    plt.bar(horas, legit_counts, label='Legit')
-    plt.xlabel('Hora del día')
-    plt.ylabel('Recuento')
-    plt.title('Recuento de fraud y legit por hora del día')
-    plt.legend()
+    for dia in categorias:
+        contador = 0
+        for item in legit_por_dia_semana:
+            if item['dia_semana'] == dia:
+                valoresl.append(item['count'])
+                contador = 1
+        if contador == 0:
+            valoresl.append(0)
 
-    ruta_png = os.path.join('reporteria', 'grafico.png')
-    plt.savefig(ruta_png)
+    drawing6 = Drawing(ancho_grafico, alto_grafico)
+    data = [valoresl, valoresf]
+    chart = VerticalBarChart()
+    chart.x = 50
+    chart.y = 50
+    chart.width = 300
+    chart.height = 100
+    chart.data = data
+    chart.categoryAxis.categoryNames = [str(h) for h in categoriasN]
+    chart.bars[0].fillColor = HexColor("#FF0000")  # Color para la categoría "Legit"
+    chart.bars[1].fillColor = HexColor("#00FF00")  # Color para la categoría "Fraud"
 
-    # Convertir el archivo PNG a PDF en la carpeta "reporteria"
-    ruta_pdf = os.path.join('reporteria', 'grafico.pdf')
-    pdfkit.from_file(ruta_png, ruta_pdf)
+    x = 0
+    drawing6.add(chart)
+    drawing6.drawOn(c, x ,y)
+    
+    numeros_semana = []
+    nombres_semana = []
+    valoresf = []
+    valoresl = []
 
-    # Leer el archivo PDF generado
-    with open(ruta_pdf, 'rb') as file:
-        pdf_content = file.read()
+    # Agregar un día extra a la fecha de fin para asegurarse de incluir la última semana completa
+    
+    final = datetime.strptime(request.data['fechaFinal'], "%Y-%m-%d") + timedelta(days=1)
 
-    # Configurar la respuesta HTTP con el archivo PDF adjunto
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="grafico.pdf"'
-    response.write(pdf_content)
+    # Iterar por cada día entre las fechas de inicio y fin
+    fecha_actual = datetime.strptime(request.data['fechaInicial'], "%Y-%m-%d")
+    while fecha_actual < final:
+        numero_semana = fecha_actual.isocalendar()[1]  # Obtener el número de semana
+        numeros_semana.append(numero_semana)
+        fecha_actual += timedelta(days=1)  # Avanzar al siguiente día
+
+    for semana in numeros_semana:
+        contador = 0
+        nombres_semana.append(f"Semana {semana}")
+        for item in fraud_por_semana:
+            if item['semana'] == semana:
+                valoresf.append(item['count'])
+                contador = 1
+        if contador == 0:
+            valoresf.append(0)
+    
+    for semana in numeros_semana:
+        contador = 0
+        for item in legit_por_semana:
+            if item['semana'] == semana:
+                valoresl.append(item['count'])
+                contador = 1
+        if contador == 0:
+            valoresl.append(0)
+    
+    drawing4 = Drawing(ancho_grafico, alto_grafico)
+    data = [valoresl, valoresf]
+    chart = VerticalBarChart()
+    chart.x = 50
+    chart.y = 50
+    chart.width = 300
+    chart.height = 100
+    chart.data = data
+    chart.categoryAxis.categoryNames = [str(h) for h in nombres_semana]
+    chart.bars[0].fillColor = HexColor("#FF0000")  # Color para la categoría "Legit"
+    chart.bars[1].fillColor = HexColor("#00FF00")  # Color para la categoría "Fraud"
+    
+    x = ancho_grafico
+    
+    drawing4.add(chart)
+    drawing4.drawOn(c, x, y)
+    
+    numeros_mes = []
+    valoresf = []
+    valoresl = []
+
+
+    # Agregar un día extra a la fecha de fin para asegurarse de incluir la última semana completa
+    final = datetime.strptime(request.data['fechaFinal'], "%Y-%m-%d") + timedelta(days=1)
+
+
+    # Iterar por cada día entre las fechas de inicio y fin
+    fecha_actual = datetime.strptime(request.data['fechaInicial'], "%Y-%m-%d").replace(day=1)
+
+    while fecha_actual < final:
+        numero_mes = fecha_actual.month  # Obtener el número de mes
+        numeros_mes.append(numero_mes)
+        fecha_actual = fecha_actual.replace(day=1)  # Avanzar al primer día del siguiente mes
+
+        # Calcular el siguiente mes
+        if fecha_actual.month == 12:
+            fecha_actual = fecha_actual.replace(year=fecha_actual.year + 1, month=1)
+        else:
+            fecha_actual = fecha_actual.replace(month=fecha_actual.month + 1)
+
+    
+    for mes in numeros_mes:
+        contador = 0
+        for item in fraud_por_mes:
+            if item['mes'] == mes:
+                valoresf.append(item['count'])
+                contador = 1
+        if contador == 0:
+            valoresf.append(0)
+    
+    for mes in numeros_mes:
+        contador = 0
+        for item in legit_por_mes:
+            if item['mes'] == mes:
+                valoresl.append(item['count'])
+                contador = 1
+        if contador == 0:
+            valoresf.append(0)
+    
+    drawing5 = Drawing(ancho_grafico, alto_grafico)
+    data = [valoresl, valoresf]
+    chart = VerticalBarChart()
+    chart.x = 50
+    chart.y = 50
+    chart.width = 300
+    chart.height = 100
+    chart.data = data
+    chart.categoryAxis.categoryNames = [str(h) for h in numeros_mes]
+    chart.bars[0].fillColor = HexColor("#FF0000")  # Color para la categoría "Legit"
+    chart.bars[1].fillColor = HexColor("#00FF00")  # Color para la categoría "Fraud"
+
+    x = ancho_grafico * 2
+    
+    drawing5.add(chart)
+    drawing5.drawOn(c, x, y)
+    
+    c.showPage()
+    c.save()
+    
+    with open('reporteria/graficos.pdf', 'rb') as file:
+        response = HttpResponse(file.read(), content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="graficos.pdf"'
 
     return response
